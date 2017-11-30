a6a06080d202b81b273223f84fe1acc4c9db6762
liu738633885 <738633885@qq.com>
2017-11-30 11:15:06 +0800

    终极大更新

--- app/src/main/java/com/hyphenate/chatuidemo/ui/ConversationListFragment.java
+++ app/src/main/java/com/hyphenate/chatuidemo/ui/ConversationListFragment.java
@@ -118,25 +118,26 @@ public class ConversationListFragment extends EaseConversationListFragment{
         super.onConnectionDisconnected();
         if (NetUtils.hasNetwork(getActivity())){
          errorText.setText(R.string.can_not_connect_chat_server_connection);
+            EMClient.getInstance().login(UserManager.getId() + "", "123456", new EMCallBack() {
+                @Override
+                public void onSuccess() {
+
+                }
+
+                @Override
+                public void onError(int i, String s) {
+
+                }
+
+                @Override
+                public void onProgress(int i, String s) {
+
+                }
+            });
         } else {
           errorText.setText(R.string.the_current_network);
         }
-        EMClient.getInstance().login(UserManager.getId() + "", "123456", new EMCallBack() {
-            @Override
-            public void onSuccess() {
 
-            }
-
-            @Override
-            public void onError(int i, String s) {
-
-            }
-
-            @Override
-            public void onProgress(int i, String s) {
-
-            }
-        });
     }
     
     
